5b3cae6cd5a1439e01f9c8f946adbf3132a89109
Katiew8 <katiejane270@gmail.com>
2023-04-09 20:31:37 -0600

    backend

---
+++ movieAPI/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using movieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MoviesDbContext context;
+        public MovieController(MoviesDbContext temp) {
+            context = temp;
+        }
+        public IEnumerable<Movie> Get()
+        {
+            var x = context.Movies
+                .Where(m => m.Edited == "yes")
+                .OrderBy(m => m.Title)
+                .ToArray();
+
+            return x;
+               
+        }
+
+    }
+}
